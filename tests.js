1f9a0261ccb561c666d89c435aceb21fa1632e4e
Rail Khusnutdinov <mail@rail-ka.ru>
2019-09-25 15:36:11 +0300

    First commit

---
+++ tests.js
@@ -0,0 +1,14 @@
+const { assert } = require('chai');
+const { it, describe } = require('mocha')
+
+/*
+* написать тест, который вызывает
+* ssh user:pass@host,
+* cd /etc
+* ls | grep deb
+* get debian_version
+* put /path/to/localfile
+* TAB filena...
+* exit
+* */
+
